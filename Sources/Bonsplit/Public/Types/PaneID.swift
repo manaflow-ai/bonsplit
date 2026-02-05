cf827c92fb91c5f2f828e310733a68cc9b49b11f
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2026-02-05 03:06:37 -0800

    Make PaneID.id and init(id:) public, add CustomStringConvertible
    Allow host apps to look up panes by UUID for socket API commands.

--- Sources/Bonsplit/Public/Types/PaneID.swift
+++ Sources/Bonsplit/Public/Types/PaneID.swift
@@ -1,14 +1,18 @@
 import Foundation
 
 /// Opaque identifier for panes
-public struct PaneID: Hashable, Codable, Sendable {
-    internal let id: UUID
+public struct PaneID: Hashable, Codable, Sendable, CustomStringConvertible {
+    public let id: UUID
 
     public init() {
         self.id = UUID()
     }
 
-    internal init(id: UUID) {
+    public init(id: UUID) {
         self.id = id
+    }
+
+    public var description: String {
+        id.uuidString
     }
 }
